2c9a85dc4bac72ff08fd69ec88a5128980a8aba6
Alyssa Dierking <a@dierking.me>
2026-07-15 21:06:07 -0700

    Version 0.1.0b3

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ project = "Picopod"
 copyright = "2026, Alyssa Dierking"
 author = "Alyssa Dierking"
 version = "0.1"
-release = "0.1.0b2"
+release = "0.1.0b3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
